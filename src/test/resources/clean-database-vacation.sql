3bd12109c11ef5c7ab967d44a4c172f472c42360
eudes.justino <eudes.justino@accenture.com>
2019-10-31 09:52:14 -0300

    deletando arquivos desnecessário.

--- src/test/resources/clean-database-vacation.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM vacation;
-DELETE FROM employee;
-DELETE FROM team;
-
---ALTER SEQUENCE telefone_id_seq RESTART WITH 1;
-
-
--- src/test/resources/load-database-team.sql
+++
@@ -1,3 +0,0 @@
-insert INTO team (id, name) VALUES (1, 'DEV');
-insert INTO team (id, name) VALUES (2, 'TESTE');
-insert INTO team (id, name) VALUES (3, 'MENAGER');
